a6a2e2df31ed6ad8365888b327dad7d72113411f
EmaLau <emanuelelauro69@gmail.com>
2022-05-18 01:14:38 +0200

    .

--- CRC/CRC/main.cpp
+++ CRC/CRC/main.cpp
@@ -18,9 +18,6 @@
  - scrivere funzione prende in input la codeword/k e stampa se rileva errori o meno, immediatamente al di sotto della stampa degli errori eventualmente introdotti del punto precedente
  */
 
-
-
-
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
@@ -30,96 +27,99 @@
 
 using namespace std;
 
-string genera_sequenza_bits_da_trasmettere(int nr_bits) {
-    string bits(""); //classe string
+string genera_sequenza_bits_da_trasmettere(int nr_bits)
+{
+    string bits(""); // classe string
     int random_bit_value = 0;
     srand(time(NULL));
-    for(int i=0; i<nr_bits; i++){
-        random_bit_value = rand() %2;
+    for (int i = 0; i < nr_bits; i++)
+    {
+        random_bit_value = rand() % 2;
         bits.append(to_string(random_bit_value));
     }
-    
+
     return bits;
 }
 
-string divide(int n, int g, string temp, string gen) {
-    for (int i=0; i<n ; i++) {
-        if (gen[0] == temp[i]) {
-            for (int j=0, k=i; j < g+1; j++, k++) {
-                if (temp[k]==gen[j])
-                    temp[k]='1';
-                else if (temp[k]!=gen[j])
-                    temp[k]='0';
+string divide(int n, int g, string temp, string gen)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (gen[0] == temp[i])
+        {
+            for (int j = 0, k = i; j < g + 1; j++, k++)
+            {
+                if (temp[k] == gen[j])
+                    temp[k] = '1';
+                else if (temp[k] != gen[j])
+                    temp[k] = '0';
             }
         }
     }
-    
+
     return temp;
 }
 
-
-
-
-
-
-int main(int argc, const char * argv[]) {
-    //Ema & Amir
+int main(int argc, const char *argv[])
+{
+    // Ema & Amir
     int nr_bits_da_trasmettere;
     string scelta;
     nr_bits_da_trasmettere = 8;
     /*cout<<"Inserire bits da 8 a 32: ";
     cin>>nr_bits_da_trasmettere; */
-    if(nr_bits_da_trasmettere > 7 && nr_bits_da_trasmettere < 33)
-        cout<<"numero bits da trasmettere: "<<nr_bits_da_trasmettere<<endl;
-    else{
-        cout<<"Numero bits da trasmettere fuori range (8-32): "<<nr_bits_da_trasmettere<<endl;
+    if (nr_bits_da_trasmettere > 7 && nr_bits_da_trasmettere < 33)
+        cout << "numero bits da trasmettere: " << nr_bits_da_trasmettere << endl;
+    else
+    {
+        cout << "Numero bits da trasmettere fuori range (8-32): " << nr_bits_da_trasmettere << endl;
         return 0;
     }
-    //Amir & Emanuele
+    // Amir & Emanuele
     string Mx("");
-    
-    //Emanuele
+
+    // Emanuele
     Mx = genera_sequenza_bits_da_trasmettere(nr_bits_da_trasmettere);
-    cout<<"Bits da trasmettere (generati casualmente): ";
-    for (int i=0; i<nr_bits_da_trasmettere; i++)
-        cout<< Mx[i];
-    cout<<endl;
-    
+    cout << "Bits da trasmettere (generati casualmente): ";
+    for (int i = 0; i < nr_bits_da_trasmettere; i++)
+        cout << Mx[i];
+    cout << endl;
+
     string divisore;
-    
-    cout<<"Inserisci Gx: ";
-    cin>>divisore;
-    
+
+    cout << "Inserisci Gx: ";
+    cin >> divisore;
+
     int lunghezza_divisore = divisore.length();
-    if(lunghezza_divisore<nr_bits_da_trasmettere-2)
-        cout<<"Gx valido, corrisponde a "<<divisore<<" e la sua lunghezza e' "<<lunghezza_divisore<<" Bits"<<endl;
-    else{
-        cout<<"Gx non valido, corrisponde a "<<divisore<<" e la sua lunghezza e' "<<lunghezza_divisore<<" Bits, dovrebbe essere massimo "<< nr_bits_da_trasmettere-2 <<" Bits"<<endl;
+    if (lunghezza_divisore < nr_bits_da_trasmettere - 2)
+        cout << "Gx valido, corrisponde a " << divisore << " e la sua lunghezza e' " << lunghezza_divisore << " Bits" << endl;
+    else
+    {
+        cout << "Gx non valido, corrisponde a " << divisore << " e la sua lunghezza e' " << lunghezza_divisore << " Bits, dovrebbe essere massimo " << nr_bits_da_trasmettere - 2 << " Bits" << endl;
         return 0;
     }
-    int bits_zero_da_aggiungere_in_coda=lunghezza_divisore+1;
+    int bits_zero_da_aggiungere_in_coda = lunghezza_divisore + 1;
     for (int i = nr_bits_da_trasmettere; i < nr_bits_da_trasmettere + bits_zero_da_aggiungere_in_coda; i++)
         Mx += "0";
-    //Invia
-    
+    // Invia
+
     string sender;
-    
-    sender= divide(nr_bits_da_trasmettere, lunghezza_divisore, Mx, divisore);
-    
-    cout<<"CRC"<<endl;
-    
-    for(int i=0; i<lunghezza_divisore; i++){
-        Mx[nr_bits_da_trasmettere+1]=sender[nr_bits_da_trasmettere+1];
-        cout<<sender[nr_bits_da_trasmettere+1]<<' ';
+
+    sender = divide(nr_bits_da_trasmettere, lunghezza_divisore, Mx, divisore);
+
+    cout << "CRC" << endl;
+
+    for (int i = 0; i < lunghezza_divisore; i++)
+    {
+        Mx[nr_bits_da_trasmettere + 1] = sender[nr_bits_da_trasmettere + 1];
+        cout << sender[nr_bits_da_trasmettere + 1] << ' ';
     }
-    cout<<endl;
-    
-    cout<<"Trasmessi"<<endl;
-    for(int i=0; i<nr_bits_da_trasmettere+lunghezza_divisore; i++)
-        cout<<Mx[i]<<' ';
-    cout<<endl;
-    
-    
-    
+    cout << endl;
+
+    cout << "Trasmessi" << endl;
+    for (int i = 0; i < nr_bits_da_trasmettere + lunghezza_divisore; i++)
+        cout << Mx[i] << ' ';
+    cout << endl;
+
     return 0;
 }
