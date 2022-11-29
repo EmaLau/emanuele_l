0bd5bce43edd2ef5fddd0e67093dff9bc0b20d6a
Emalau <emanuelelauro69@gmail.com>
2022-11-29 14:13:52 +0100

    Adding old git's file

---
+++ 12-10-2022/12-10-2022/main.cpp
@@ -0,0 +1,19 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+//  char mychar = 'A';
+    char mychar = 'a';
+
+cout << "il carattere e': " << mychar;
+
+    for (int c = 0; c < 128; c++) {
+        //char mychar = 'c';
+        cout << c << " '" << mychar << "'" << endl;
+    }
+ cout<<endl;
+}
+*/
