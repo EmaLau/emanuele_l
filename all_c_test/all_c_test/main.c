0e1458fb441679fa03bf1f24199db8ca6e92f42b
EmaLau <emalau@EmaLaus-Air.local>
2023-04-03 16:54:47 +0200

    File Sistemati

--- all_c_test/all_c_test/main.c
+++ all_c_test/all_c_test/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
+#include <signal.h>
+
+void gestore(int segnale){
+    printf("...ricevuto segnale %d\n", segnale);
+    sleep(2);
+    printf("...terminazione handler segnale %d\n", segnale);
+}
 
 int main(int argc, const char * argv[]) {
+    int k;
+    signal(SIGINT, gestore);
+    for(k =1;; k++){
+        printf("%d\n", k);
+        sleep(1);
+    }
+        
     return 0;
 }
