e7355139687ec259f9c6f9799e9aa2b7ec31f6bc
EmaLau <emalau@EmaLaus-Air.local>
2023-05-22 10:56:40 +0200

    Triangoli, Junit

---
+++ 2022-2023/UD 3/Lab_1/Triangolo_JUnit/src/main/java/com/mycompany/triangolo_junit/Triangolo_JUnit.java
@@ -0,0 +1,12 @@
+package com.mycompany.triangolo_junit;
+
+/**
+ *
+ * @author emalau
+ */
+public class Triangolo_JUnit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
