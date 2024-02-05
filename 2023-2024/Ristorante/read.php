12534442cc9fdfbc4c29895269d112dc424ce7ba
EmaLau <emalau@Mac-mini-di-EmaLau.local>
2024-02-05 18:35:52 +0100

    Completato esercizio Ristorante

---
+++ 2023-2024/Ristorante/read.php
@@ -0,0 +1,19 @@
+<?php
+
+$conn = mysqli_connect("127.0.0.1", "root", "", "ristorante");
+
+if($conn === false){
+    die("ERRORE :" . mysqli_connect_error());
+}
+
+$nome = $_REQUEST['nome'];
+$cognome = $_REQUEST['cognome'];
+
+$sql = "INSERT INTO camerieri (nome, cognome) VALUES ('$nome', '$cognome')";
+if(mysqli_query($conn, $sql)){
+    echo "<h3>Data messi con successo</h3>";
+    echo nl2br("\n$nome \n$cognome");
+}else{
+    echo "ERRORE" . mysqli_error($conn);
+}
+mysqli_close($conn);
