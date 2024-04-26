1fa25cda2ef89bc3651db06aef6f3ae5d908994b
EmaLau <emalau@Mac-mini-di-EmaLau.local>
2024-04-26 22:47:52 +0200

    Noleggio

--- 2023-2024/Noleggio/main.py
+++ 2023-2024/Noleggio/main.py
@@ -1,17 +1,45 @@
-import csv
-import mariadb
-import sys
+import mysql.connector
+import pandas as pd
 
+# Connessione al database
+conn = mysql.connector.connect(
+    host="localhost",
+    user="username",
+    password="password",
+    database="Noleggio"
+)
+cursor = conn.cursor()
 
-try:
-    connection = mariadb.connect(user="root", password="", host="localhost", port=3306, database="Noleggio")
-except mariadb.Error as e:
-    print(e);
-    sys.exit(1)
+# Percorso del file CSV
+csv_file_path = "/path/to/noleggi.csv"
 
+# Carica i dati nella tabella Clienti
+df_clienti = pd.read_csv(csv_file_path)
+for _, row in df_clienti.iterrows():
+    cursor.execute("""
+        INSERT INTO Clienti (cognome, nome, data_nascita, data_rilascio_patente, num_cell, mail)
+        VALUES (%s, %s, %s, %s, %s, %s)
+    """, (row['cognome'], row['nome'], row['data_nascita'], row['data_rilas_patente'], row['num_cell'], row['mail']))
+conn.commit()
 
+# Carica i dati nella tabella Auto
+df_auto = pd.read_csv(csv_file_path)
+for _, row in df_auto.iterrows():
+    cursor.execute("""
+        INSERT INTO Auto (targa, marca, modello, tipo_alimentazione, data_revisione, costo_per_km)
+        VALUES (%s, %s, %s, %s, %s, %s)
+    """, (row['targa'], row['marca'], row['modello'], row['tipo_alimentazione'], row['data_revisione'], row['costo_per_km']))
+conn.commit()
 
-with open('noleggi.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in spamreader:
-        print(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+# Carica i dati nella tabella Noleggi
+df_noleggi = pd.read_csv(csv_file_path)
+for _, row in df_noleggi.iterrows():
+    cursor.execute("""
+        INSERT INTO Noleggi (id_cliente, id_auto, data_inizio_noleggio, data_fine_noleggio, km_effettuati)
+        VALUES (%s, %s, %s, %s, %s)
+    """, (row['id_cliente'], row['id_auto'], row['data_inizio_noleggio'], row['data_fine_noleggio'], row['km_effettuati']))
+conn.commit()
+
+# Chiudi la connessione al database
+cursor.close()
+conn.close()
